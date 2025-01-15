817ed666392f8ccde0b907c46760d1dfb81ea382
svetk0 <sv.solomatnikova@gmail.com>
2025-01-16 01:14:00 +0300

    add mutation

--- src/api/content.ts
+++ src/api/content.ts
@@ -1,30 +1,17 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 //import { IChallenge } from '@/types';
-interface User {
-  //id?: number;
-  telegram_id: string | null | number;
-  username: string | null;
-  first_name: string;
-  last_name: string;
-  email: string;
-  //photo?: null;
-}
-interface AddToken {
-  token_name: string;
-}
-interface SendToken {
-  sender: string;
-  receiver: string;
-  amount: string;
-  send_commission?: number;
+
+interface AddChallenge {
+  description: string;
+  goal: number;
+  period: string;
+  started_at: string;
+  finished_at: string;
 }
 interface ResponseUser {
-  data: User;
-  dataSend: SendToken;
-  dataAdd: AddToken;
+  dataAdd: AddChallenge;
   status: number;
   telegramId: string;
-  token_name: string;
 }
 
 export const contentApi = createApi({
@@ -33,11 +20,10 @@ export const contentApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: 'http://193.164.150.86:8098/',
   }),
-  tagTypes: ['TokenBalance', 'UserTokenList'],
+  tagTypes: ['Actual'],
 
   endpoints: (builder) => ({
     //------------   GET   ------------
-
     //user info
     getUserMe: builder.query({
       query: (telegramId: string) => ({
@@ -53,44 +39,21 @@ export const contentApi = createApi({
     //Get list of user's challenges
     getAllChallengeList: builder.query({
       query: (telegramId: string) => ({
-        url: 'challenges/?limit=100',
+        url: '/challenges/in-progress/?limit=100',
         method: 'GET',
         headers: {
           Authorization: telegramId,
         },
       }),
+      providesTags: ['Actual'],
     }),
 
     //------------   POST   ------------
-    //create user (Registration)
-    createUser: builder.mutation<ResponseUser, Partial<User>>({
-      query: (data) => ({
-        url: `users/`,
-        method: 'POST',
-        body: data,
-        headers: {
-          'content-type': 'application/json',
-        },
-      }),
-    }),
-
-    sendToken: builder.mutation<SendToken, Partial<ResponseUser>>({
-      query: ({ dataSend, telegramId }) => ({
-        url: `send/`,
-        method: 'POST',
-        body: dataSend,
-        headers: {
-          Authorization: telegramId,
-          'content-type': 'application/json',
-        },
-      }),
-      invalidatesTags: ['TokenBalance', 'UserTokenList'],
-    }),
 
     //add new token in user's wallet
-    addToken: builder.mutation<AddToken, Partial<ResponseUser>>({
+    addChallenge: builder.mutation<AddChallenge, Partial<ResponseUser>>({
       query: ({ dataAdd, telegramId }) => ({
-        url: `wallets/`,
+        url: `challenges/`,
         method: 'POST',
         body: dataAdd,
         headers: {
@@ -98,17 +61,8 @@ export const contentApi = createApi({
           'content-type': 'application/json',
         },
       }),
-      invalidatesTags: ['UserTokenList'],
-    }),
-
-    //-------   WILL BE REMOVED -------
-    //for development process
-    getAllUsers: builder.query({
-      query: () => ({
-        url: 'users/',
-        method: 'GET',
-      }),
+      invalidatesTags: ['Actual'],
     }),
   }),
 });
-export const { useGetAllChallengeListQuery } = contentApi;
+export const { useGetAllChallengeListQuery, useAddChallengeMutation } = contentApi;
