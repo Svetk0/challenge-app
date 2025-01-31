47d90ee929714d95e3a44edab7c024a55d27321d
svetk0 <sv.solomatnikova@gmail.com>
2025-01-31 21:37:51 +0300

    add static data

--- src/app/challenges/page.tsx
+++ src/app/challenges/page.tsx
@@ -1,22 +1,8 @@
-'use client';
-
-import { useRouter } from 'next/navigation';
-import { Button } from '@/components';
-import Link from 'next/link';
 import ListChallenges from '@/components/ListChallenges/ListChallenges';
-export default function Home() {
-  const router = useRouter();
+export default function Page() {
   return (
     <>
       <ListChallenges />
-      <Link href={'/challenges/create'}>
-        <Button
-          type='button'
-          text={'Add new'}
-          color='default'
-          onClick={() => router.push('/challenges/create')}
-        />
-      </Link>
     </>
   );
 }
--- src/components/ListChallenges/ListChallenges.tsx
+++ src/components/ListChallenges/ListChallenges.tsx
@@ -1,4 +1,7 @@
 'use client';
+
+import Link from 'next/link';
+//import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '@/lib/store';
@@ -6,12 +9,17 @@ import { TChallenge } from '@/types';
 import { useGetAllChallengeListQuery } from '@/api/content';
 import { setLocalStorage } from '@/utils/localStorage';
 import { setChallenges } from '@/lib/features/challenges/challengeSlice';
+import { ChallengeInfo, Button } from '@/components/';
+import staticData from '@/constants/data.json';
 import styles from './listChallenges.module.scss';
 
-import { ChallengeInfo } from '@/components/';
-
 export default function ListChallenges() {
+  //const router = useRouter();
   const dispatch = useDispatch();
+  const {
+    title,
+    buttons: { add },
+  } = staticData.challendes;
   const [local, setLocal] = useState<TChallenge[]>([]);
   const { data, error, isLoading, isSuccess } = useGetAllChallengeListQuery();
   const challengeData = useSelector((state: RootState) => state.challenge.challenges);
@@ -41,7 +49,7 @@ export default function ListChallenges() {
 
   return (
     <div className={styles.container}>
-      <h2>My Challenges List</h2>
+      <h2>{title}</h2>
       <ol className={styles.list}>
         {data?.map((item: TChallenge) => (
           <li key={`challenge-${item.id}`}>
@@ -49,6 +57,14 @@ export default function ListChallenges() {
           </li>
         ))}
       </ol>
+      <Link href={'/challenges/create'}>
+        <Button
+          type='button'
+          text={add}
+          color='default'
+          //onClick={() => router.push('/challenges/create')}
+        />
+      </Link>
     </div>
   );
 }
