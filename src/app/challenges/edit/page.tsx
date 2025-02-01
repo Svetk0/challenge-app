afe3c5a44742817b438e6f5e3cf668c687fbcccf
svetk0 <sv.solomatnikova@gmail.com>
2025-02-01 22:43:06 +0300

    fix build bug

--- src/app/challenges/edit/page.tsx
+++ src/app/challenges/edit/page.tsx
@@ -3,10 +3,15 @@ import { useParams } from 'next/navigation';
 import EditForm from '@/components/EditForm/EditForm';
 
 export default function Page() {
-  const { id } = useParams(); // Get the id from the URL parameters
+  const params = useParams();
+  const id = params?.id; // безопасное получение id
 
   // Ensure id is a number
-  const challengeId = Array.isArray(id) ? parseInt(id[0], 10) : parseInt(id, 10);
+  const challengeId = Array.isArray(id)
+    ? parseInt(id[0], 10)
+    : typeof id === 'string'
+      ? parseInt(id, 10)
+      : 0; // или другое значение по умолчанию
 
   return <EditForm id={challengeId} />;
 }
