7b2773b86797be9319aab37954301f52d28d428c
svetk0 <sv.solomatnikova@gmail.com>
2025-02-14 01:01:46 +0300

    add upcoming date

--- src/components/ChallengeInfo/ChallengeInfo.tsx
+++ src/components/ChallengeInfo/ChallengeInfo.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { useOutsideClick } from '@/hooks/useOutsideClick';
 import { TChallenge } from '@/types';
+import { formatDate } from '@/utils';
 import { EditIcon } from '@/components/ui/Icons/';
 import { Button, ProgressBar } from '@/components';
 import styles from './ChallengeInfo.module.scss';
@@ -15,6 +16,7 @@ type Props = {
 export function ChallengeInfo({ isLoading, challenge }: Props) {
   const router = useRouter();
   const [isChoosen, setIsChoosen] = useState<boolean>(false);
+  const [isStarted, setIsStarted] = useState<boolean>(true);
   const wrapperRef = useOutsideClick(() => setIsChoosen(false));
 
   const scrollToCenter = () => {
@@ -29,8 +31,16 @@ export function ChallengeInfo({ isLoading, challenge }: Props) {
     }, 100);
   };
 
-  useEffect(() => {}, [isChoosen]);
-
+  useEffect(() => {}, [isChoosen, isStarted]);
+  useEffect(() => {
+    if (challenge) {
+      const today = new Date();
+      const startDate = new Date(challenge.started_at);
+      const diff = today.getTime() - startDate.getTime();
+      if (diff < 0) setIsStarted(false);
+      else setIsStarted(true);
+    }
+  }, [challenge]);
   if (isLoading || !challenge) {
     return <div>Loading...</div>;
   }
@@ -44,7 +54,13 @@ export function ChallengeInfo({ isLoading, challenge }: Props) {
     <div
       ref={wrapperRef}
       id={`challenge-${challenge?.uuid}`}
-      className={isChoosen ? `${styles.wrapper} ${styles.wrapper__active}` : `${styles.wrapper}`}
+      className={
+        isChoosen
+          ? `${styles.wrapper} ${styles.wrapper__active}`
+          : isStarted
+            ? `${styles.wrapper}`
+            : `${styles.wrapper} ${styles.wrapper__upcoming}`
+      }
       onClick={() => {
         setIsChoosen(true);
         scrollToCenter();
@@ -60,7 +76,17 @@ export function ChallengeInfo({ isLoading, challenge }: Props) {
         />
       </div>
       <div className={styles.rowWrapper}>
-        <div className={styles.period}>{isChoosen ? '' : `every ${period}`}</div>
+        <div className={styles.period}>
+          {isChoosen ? (
+            ''
+          ) : isStarted ? (
+            `every ${period}`
+          ) : (
+            <>
+              starts at <br /> {formatDate(challenge.started_at)}
+            </>
+          )}
+        </div>
         <ProgressBar challenge={challenge} isMinimal={!isChoosen} />
       </div>
     </div>
