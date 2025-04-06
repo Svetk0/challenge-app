b01bcfb8d627c45d0141972bfb7dd4799be3e963
svetk0 <sv.solomatnikova@gmail.com>
2025-04-06 15:26:27 +0300

    add new styles and fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import StoreProvider from './StoreProvider';
-import localFont from 'next/font/local';
+import { Sniglet, Open_Sans } from 'next/font/google';
+//import localFont from 'next/font/local';
 import { Suspense } from 'react';
 import { Footer } from '@/widgets';
 import { Root } from '@/shared/core';
@@ -8,12 +9,28 @@ import { Loading } from '@/shared/ui';
 
 import './_assets/globals.scss';
 
-const openSans = localFont({
-  src: '../../public/fonts/OpenSans.woff',
-  variable: '--font-geist-mono',
-  weight: '300 400 600',
+export const snigletFont = Sniglet({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-sniglet',
+  weight: ['400'],
 });
-
+export const openSansFont = Open_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-open-sans',
+  weight: ['400', '600'],
+});
+// const openSans = localFont({
+//   src: '../../public/fonts/OpenSans.woff',
+//   variable: '--font-geist-mono',
+//   weight: '300 400 600',
+// });
+// const bebasNeueFontLocal = localFont({
+//   src: '../../public/fonts/BebasNeue-Bold.woff2',
+//   variable: '--font-babas-neue',
+//   weight: '400',
+// });
 export const metadata: Metadata = {
   title: {
     template: '%s | My Challenges',
@@ -28,11 +45,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang='en' className={`${snigletFont.variable} ${openSansFont.variable}`}>
       <StoreProvider>
-        <body className={`${openSans.className}`}>
+        <body>
           <Root>
             <Suspense fallback={<Loading />}>
+              <div>Русский язык</div>
               <main>{children}</main>
               <Footer />
             </Suspense>
